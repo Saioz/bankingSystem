e3278d5c2e300f575029097e1913df154ab87c90
Axel PM <60494491+That1guy129@users.noreply.github.com>
2026-03-21 15:32:24 -0400

    Implement constructor and update display method

--- BankAccount.h
+++ BankAccount.h
@@ -23,11 +23,28 @@ class BankAccount {
     
     public:
     
-    BankAccount(const string& name, int accNum, double intitialDeposit); //Constructor
+    BankAccount(const string& name, int accNum, double intitialDeposit) { //Constructor
+        ownerName = name;
+        accountNumber = accNum;
+        balance = intitialDeposit;
+
+        cout << fixed << setprecision(2);
+        string msg = "Account created with intitial deposit $" + to_string(balance);
+        history.push_back(msg);
+    }
     void deposit(double amount);    // Adding to balance and gets recorded to history
     bool withdraw(double amount);   // Subracts to balance and if valid gets recorded to history
-    double getBalance() const;      //Returns current balance
-    void display() const;           // Printing account info
+    double getBalance() const { //Returns current balance
+        return balance;
+    }
+    
+void display() const { // Printing account info 
+    cout << fixed << setprecision(2);
+    cout << "Owner Name: " << ownerName <<endl;
+    cout << "Account Number: " << acountNumber <<endl;
+    cout << "Balance: $" <<balance <<endl;
+}
+
     void showHistory() const;       // Printing transaction history
 };
 
