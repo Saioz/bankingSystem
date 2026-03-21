cd68784fc386a84b2ae307c7397ab2cb558bf728
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-21 15:01:58 -0400

    added cin, fail catch loop

--- BankingSystem.cpp
+++ BankingSystem.cpp
@@ -15,6 +15,24 @@ using namespace std;
 
 
 int main() {
-  cout << "======== <  Banking System > ========";
+  cout << "======== <  Banking System > ========" << endl;
+  // print the banking system name, making it look nice!
 
+
+  string name;
+  cout << "Please enter account name: ";
+  getline(cin, name);
+  //there's no need for a cin.fail check here, since a string can be anything
+
+
+  // menu options
+
+  if(cin.fail()) {
+    cin.clear();
+    cin.ignore(1000,'\n');
+    cout << "Invalid, Please re-enter menu option :";
+    continue;
+
+
+  }
 }
