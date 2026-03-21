ea4812288bc39662e399dfe4667071c4ddf04798
yusufkayes-crypto <yusufkayes@gmail.com>
2026-03-21 17:08:23 -0400

    added functiions

--- BankAccount.h
+++ BankAccount.h
@@ -1,4 +1,4 @@
-#ifndef BANKACCOUNT_H
+#ifndef BANKACCOUNT_H // best practice for .h files to stop it from running twice
 #define BANKACCOUNT_H
 
 #include <iostream>
@@ -8,45 +8,78 @@
 
 using namespace std;
 
-
-
 class BankAccount {
-    private:                    // Private Encapsualtion - These members are hidden from the outside acces
-    
-    string ownerName;           //Stores string of the account holder
-    int accountNumber;          // Stores int for the unique ID for the holder's bankaccount
-    double balance;             // Stores the current money amount (uses decimal point values)
-    vector <string> history;    // A dynamic list to save every successful transaction message
-    
-    
+private:// Private Encapsualtion - These members are hidden from the outside access
+    string ownerName;//Stores string of the account holder
+    int accountNumber; //stores int for account number
+    double balance; //stores initialDeposit and current balance as a double
+    vector<string> history;//a vector to keep track of past transactions
 
-    
-    public:
-    
-    BankAccount(const string& name, int accNum, double intitialDeposit) { //Constructor
+public:
+    BankAccount(const string& name, int accNum, double initialDeposit) {
         ownerName = name;
         accountNumber = accNum;
-        balance = intitialDeposit;
+        balance = initialDeposit;
+        string text = to_string(initialDeposit);
+        text = text.substr(0, text.find('.') + 3);
+
+        history.push_back("Account created with initial deposit $" + text);
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        
+        string text = to_string(amount);
+        text = text.substr(0, text.find('.') + 3); //stores with 2 decimal
+
+        history.push_back("Deposited $" + text);
 
         cout << fixed << setprecision(2);
-        string msg = "Account created with intitial deposit $" + to_string(balance);
-        history.push_back(msg);
+        cout << "Deposited $" << amount << endl;
     }
-    void deposit(double amount);    // Adding to balance and gets recorded to history
-    bool withdraw(double amount);   // Subracts to balance and if valid gets recorded to history
-    double getBalance() const { //Returns current balance
+
+    bool withdraw(double amount) {
+        if (amount > balance) {
+            cout << "Insufficient funds." << endl;
+            return false;
+        }
+
+        balance -= amount;
+        
+        string text = to_string(amount);
+        text = text.substr(0, text.find('.') + 3);
+
+        history.push_back("Withdrew $" + text);
+
+        cout << fixed << setprecision(2);
+        cout << "Withdrew $" << amount << endl;
+
+        if (balance == 0) {
+            cout << "Balance is now zero." << endl;
+        }
+
+        return true;
+    }
+
+    double getBalance() const {
         return balance;
     }
-    
-void display() const { // Printing account info 
-    cout << fixed << setprecision(2);
-    cout << "Owner Name: " << ownerName <<endl;
-    cout << "Account Number: " << acountNumber <<endl;
-    cout << "Balance: $" <<balance <<endl;
-}
 
-    void showHistory() const;       // Printing transaction history
+    void display() const {
+        cout << fixed << setprecision(2);
+        cout << "\n=== Account Information ===" << endl;
+        cout << "Owner Name: " << ownerName << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Balance: $" << balance << endl;
+    }
+
+    void showHistory() const {
+        cout << "\nTransaction History (" << history.size() << "):" << endl;
+
+        for (int i = 0; i < history.size(); i++) {
+            cout << "- " << history[i] << endl;
+        }
+    }
 };
 
 #endif
-
