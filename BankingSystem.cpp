3af00200270a0340719febc1b1ab1ca864f771b0
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-21 01:11:04 -0400

    //drafted edits

--- BankingSystem.cpp
+++ BankingSystem.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+/*
+cout << "hi, how are you?" << endl;
+cout << "would you like to test out our bankingSystem!?" << endl;
+
+cin >> bankBool{};
+while(cin.fail) {
+    cin.clear()
+    cin.ignore(1000, "");
+    cin >> bankBool{};
+}
+
+if (bankBool) {
+
+    cout << "That's great! let's get a started then." << endl;
+} 
+else {
+    cout << "aw :( that's too bad....)";
+}
+
+*/
+
+
+
+
+
+// this is random stuff for now, we need to make it work first, and then work on the aesthetics...
+}
