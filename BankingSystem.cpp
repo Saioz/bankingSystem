21df93ed5cafc545e55799dc4690cac1ce974f6f
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-21 15:26:14 -0400

    added switch, while loop checker

--- BankingSystem.cpp
+++ BankingSystem.cpp
@@ -11,9 +11,6 @@ using namespace std;
 
 
 
-
-
-
 int main() {
   cout << "======== <  Banking System > ========" << endl;
   // print the banking system name, making it look nice!
@@ -24,15 +21,61 @@ int main() {
   getline(cin, name);
   //there's no need for a cin.fail check here, since a string can be anything
 
-
+  
+  int choice;
+  //initializes the choice for the while loop menu. ** using case switch to make it easier than if/else 
+  while (true) {
   // menu options
-
+    cout << "\nMenu:" << endl;
+    cout << "1) Deposit:" << endl;
+    cout << "2) Withdraw:" << endl;
+    cout << "3) Show Balance:" << endl;
+    cout << "4) Show Account Info:" << endl;
+    cout << "5) Show Transaction History:" << endl;
+    cout << "6) -Exit-" << endl;
+    cout << "Choose option: ";
+  
+    cin >> choice;
+    //stores the choice 
   if(cin.fail()) {
     cin.clear();
     cin.ignore(1000,'\n');
     cout << "Invalid, Please re-enter menu option :";
     continue;
+  }
+  cin.ignore(1000, '\n');
+
+  }
+  switch (choice) {
+    case 1: 
+    cout << "case1"<< endl;
+    break;
+
+    case 2: 
+    cout << "case2"<< endl;
+    break;
+
+    case 3:
+    cout << "case3"<< endl;
+    break;
+
+    case 4:
+    cout << "case4"<< endl;
+    break;
+
+    case 5:
+    cout << "case5"<< endl;
+    break;
+
+    case 6: 
+    cout << "case6" << endl;
+    break;
+
+    default: 
+    cout << "this is default, please select a valid menu option!" << endl;
+    break;
 
 
   }
+
 }
